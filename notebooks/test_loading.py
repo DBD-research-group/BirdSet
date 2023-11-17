033c2225a9d918bf53cfc6897f94becb968b0239
Lukas Rauch <lukas.rauch@uni-kassel.de>
2023-11-17 17:46:37 +0100

    start multilabel classification. now stuck at changing the model heads for training and testing.. (multiclass --> multilabel)

--- notebooks/test_loading.py
+++ notebooks/test_loading.py
@@ -2,7 +2,7 @@
 from datasets import load_dataset
 
 if __name__ == "__main__":    
-    ds_name = "columbia_costa_rica"
+    ds_name = "high_sierras"
     print(f"start {ds_name}")
     ds = load_dataset(
         "dbd-research-group/gadme_v1", 
@@ -27,14 +27,70 @@ ds["train"][0]
 # %%
 
 from huggingface_hub import hf_hub_download
+import pandas as pd 
 
-meta = hf_hub_download(
+folder = "data/xenocanto"
+meta0 = hf_hub_download(
+    repo_id="dbd-research-group/gadme_v1",
+    filename="train.parquet",
+    revision="data",
+    subfolder=folder,
+    repo_type="dataset"
+)
+
+meta1 = hf_hub_download(
     repo_id="dbd-research-group/gadme_v1",
     filename="test.parquet",
     revision="data",
-    subfolder="data/amazon_basin",
+    subfolder=folder,
     repo_type="dataset"
 )
+
+# meta2 = hf_hub_download(
+#     repo_id="dbd-research-group/gadme_v1",
+#     filename="test_5s.parquet",
+#     revision="data",
+#     subfolder=folder,
+#     repo_type="dataset"
+# )
+print(pd.read_parquet(meta0)["ebird_code"].nunique())
+print(pd.read_parquet(meta1)["ebird_code"].nunique())
+#print(pd.read_parquet(meta2)["ebird_code"].nunique())
+
+#%%
+from huggingface_hub import hf_hub_download
+import pandas as pd 
+
+folder = "data/amazon_basin"
+meta_test = hf_hub_download(
+    repo_id="dbd-research-group/gadme_v1",
+    filename="train.parquet",
+    revision="data",
+    subfolder=folder,
+    repo_type="dataset"
+)
+#%%
+meta_test = pd.read_parquet(meta_test)
+#%%
+meta_test
+#%%
+col_index = meta_test.columns.get_loc("ebird_code") + 1
+meta_test.insert(col_index, "ebird_code_multiclass", meta_test["ebird_code"])
+meta_test["ebird_code"] = None
+meta_test.insert(col_index+2, "ebird_code_secondary", None)
+#%%
+
+meta_test
+#%%
+df = pd.read_parquet(meta0)
+#%%
+len(df["ebird_code"].unique())
+#%%
+df.head()
+#%%
+from datasets import load_dataset
+ds = load_dataset("dbd-research-group/gadme_v1", "iit")
+
 #%%
 import pandas as pd 
 
