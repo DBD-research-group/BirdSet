b0699eaebdc54dca50f00cebc51161549fc0d61c
lurauch <75244861+lurauch@users.noreply.github.com>
2024-01-15 17:37:43 +0100

    Revert "New sampling method for events and class limits"

--- src/datamodule/gadme_datamodule.py
+++ src/datamodule/gadme_datamodule.py
@@ -30,7 +30,7 @@ class GADMEDataModule(BaseDataModuleHF):
     def _preprocess_data(self, dataset):
         if self.dataset_config.task == "multiclass":
             # pick only train and test dataset
-            dataset = DatasetDict({split: dataset[split] for split in ["train", "valid", "test"]})
+            dataset = DatasetDict({split: dataset[split] for split in ["train", "test"]})
 
             logging.info("> Mapping data set.")
             dataset["train"] = dataset["train"].map(
@@ -42,31 +42,14 @@ class GADMEDataModule(BaseDataModuleHF):
                 num_proc=self.dataset_config.n_workers,
             )
 
-            dataset["valid"] = dataset["valid"].map(
-                self.event_mapper,
-                remove_columns=["audio"],
-                batched=True,
-                batch_size=300,
-                load_from_cache_file=True,
-                num_proc=self.dataset_config.n_workers,
-            )
-
             if self.dataset_config.class_weights_loss or self.dataset_config.class_weights_sampler:
                 self.num_train_labels = self._count_labels((dataset["train"]["ebird_code"]))
             
-            if self.dataset_config.classlimit and not self.dataset_config.eventlimit:
+            if self.dataset_config.classlimit:
                 dataset["train"] = self._limit_classes(
                     dataset=dataset["train"],
                     label_name="ebird_code",
-                    limit=self.dataset_config.classlimit
-                )
-            elif self.dataset_config.classlimit or self.dataset_config.eventlimit:
-                dataset["train"] = self._smart_sampling(
-                    dataset=dataset["train"],
-                    label_name="ebird_code",
-                    class_limit=self.dataset_config.classlimit,
-                    event_limit=self.dataset_config.eventlimit
-                )
+                    limit=self.dataset_config.classlimit)
 
             dataset = dataset.rename_column("ebird_code", "labels")
 
@@ -98,16 +81,15 @@ class GADMEDataModule(BaseDataModuleHF):
                 self.num_train_labels = self._count_labels((dataset["train"]["ebird_code"]))
 
             dataset["test"] = dataset["test_5s"]
+            
 
         dataset["train"] = dataset["train"].select_columns(
             ["filepath", "labels", "detected_events", "start_time", "end_time", "no_call_events"]
         )
-        dataset["valid"] = dataset["valid"].select_columns(
-            ["filepath", "labels", "detected_events", "start_time", "end_time", "no_call_events"]
-        )
         # maybe has to be added to test data to avoid two selections
-        dataset["test"] = dataset["test"].select_columns(
+        dataset["test"]= dataset["test"].select_columns(
             ["filepath", "labels", "detected_events", "start_time", "end_time"]
         )
 
         return dataset
+        
