3965ad1ad75f44608f987fb6d040b99ef77d0e6b
lurauch <lukas.rauch@uni-kassel.de>
2024-01-21 15:35:00 +0100

    fix multilabel metrics

--- src/modules/metrics/loader.py
+++ src/modules/metrics/loader.py
@@ -8,15 +8,15 @@ def load_metrics(metrics_cfg: DictConfig):
     main_metric = hydra.utils.instantiate(metrics_cfg.main)
     val_metric_best = hydra.utils.instantiate(metrics_cfg.val_best)
 
-    additional_metrics = []
+    additional_metrics = {}
     if metrics_cfg.get("additional"):
-        for _, metric in metrics_cfg.additional.items():
-            additional_metrics.append(hydra.utils.instantiate(metric))
+        for name, metric in metrics_cfg.additional.items():
+            additional_metrics[name] = hydra.utils.instantiate(metric)
     
-    eval_metrics_complete = []
+    eval_metrics_complete = {}
     if metrics_cfg.get("eval_complete"):
-        for _, metric in metrics_cfg.eval_complete.items():
-            eval_metrics_complete.append(hydra.utils.instantiate(metric))
+        for name, metric in metrics_cfg.eval_complete.items():
+            eval_metrics_complete[name] = hydra.utils.instantiate(metric)
     
     metrics = {
         "main_metric": main_metric, 
