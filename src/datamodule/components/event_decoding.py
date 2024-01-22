6f8ea90534c9bb2ad8dc9756c6a60b41041f4b40
lurauch <lukas.rauch@uni-kassel.de>
2024-01-22 14:19:39 +0100

    add random time shifting to event decoding

--- src/datamodule/components/event_decoding.py
+++ src/datamodule/components/event_decoding.py
@@ -5,13 +5,14 @@ import soundfile as sf
 import librosa
 
 class EventDecoding:
-    def __init__(self, min_len=1, max_len=None, sampling_rate=None):
+    def __init__(self, min_len=1, max_len=None, sampling_rate=None, extension_time=7, extracted_interval=5):
         self.min_len = min_len # in seconds
         self.max_len = max_len
         self.sampling_rate = sampling_rate
+        self.extension_time = extension_time
+        self.extracted_interval = extracted_interval
 
-    def _load_audio(self, path, start=None, end=None):
-        sr = sf.info(path).samplerate
+    def _load_audio(self, path, start=None, end=None, sr=None):
         if start is not None and end is not None:
             if end - start < self.min_len:  # TODO: improve, eg. edge cases, more dynamic loading
                 end = start + self.min_len
@@ -30,20 +31,53 @@ class EventDecoding:
             audio = librosa.resample(audio, orig_sr=sr, target_sr=self.sampling_rate)
             sr = self.sampling_rate
         return audio, sr
+    
+    def _time_shifting(self, start, end, total_duration):
+        event_duration = round(end - start, 2)
+
+        if event_duration < self.extension_time:
+            side_extension_time = (self.extension_time - event_duration) / 2
+            new_start_time = round(max(0, start - side_extension_time))
+            new_end_time = round(min(total_duration, end + side_extension_time))
+
+            if new_end_time - new_start_time < self.extension_time:
+                if new_start_time == 0:
+                    new_end_time = min(self.extension_time, total_duration)
+                elif new_end_time == total_duration: 
+                    new_start_time = max(0, total_duration - self.extension_time)
+        
+        else: # longer than extraction time
+            new_start_time = start
+            new_end_time = end
+        
+        # select a random interval of 5 seconds
+        max_start_interval = round(new_end_time - self.extracted_interval,2)
+        random_start = round(random.uniform(new_start_time, max_start_interval),2) 
+        random_end = round(random_start + self.extracted_interval,2)
+
+        return random_start, random_end
+    
 
     def __call__(self, batch):
         audios, srs = [], []
         for b_idx in range(len(batch.get("filepath", []))):
-            if batch["detected_events"][b_idx]:
+            file_info = sf.info(batch["filepath"][b_idx])
+            sr = file_info.samplerate
+            duration = file_info.duration
+
+            if batch["detected_events"][b_idx]: #only for train data, not test
                 start, end = batch["detected_events"][b_idx]
+                #time shifting
+                start, end = self._time_shifting(start, end, duration)
             else:
                 start, end = batch["start_time"][b_idx], batch["end_time"][b_idx]
-            audio, sr = self._load_audio(batch["filepath"][b_idx], start, end)
+            audio, sr = self._load_audio(batch["filepath"][b_idx], start, end, sr)
             audios.append(audio)
             srs.append(sr)
         if batch.get("filepath", None):
             batch["audio"] = [{"path": path, "array": audio, "samplerate": sr} for audio, path, sr in zip(audios, batch["filepath"], srs)]
         return batch
+    
     
 class EventDecoding_:
     def __init__(
