b61d219bb55878d28b2ed100d26dbe52d9408fb0
Moritz Richert <mo01010010itz@protonmail.com>
2024-02-11 17:30:50 +0100

    chirp test

--- dataset/embeddings/create_embedding_dataset.py
+++ dataset/embeddings/create_embedding_dataset.py
@@ -1,7 +1,7 @@
-from chirp import inference as i
+from chirp.inference import models
 
 print("Hallo Welt")
 # models.model_class_map()["tfhub_model"]
 
-i.models.model_class_map()["tfhub_model"]
+print(models.model_class_map()["tfhub_model"])
 
