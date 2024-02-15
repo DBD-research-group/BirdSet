12a93004403fa68d2d8d6c6a8b82fc9a692cbf66
Moritz Richert <mo01010010itz@protonmail.com>
2024-02-15 14:30:48 +0100

    make modules importable

--- src/modules/models/__init__.py
+++ src/modules/models/__init__.py
@@ -1,5 +1,5 @@
 from . import wav2vec2
 from . import hubert
 from . import ast 
-from logistic_regression_embedding_models import PerchModel, BirdNetModel, BirdNetTFLiteModel
+# from logistic_regression_embedding_models import PerchModel, BirdNetModel, BirdNetTFLiteModel
 #from ..base_module import *
