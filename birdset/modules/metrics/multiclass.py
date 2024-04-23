480322f0d98688b2c5d1719da7cb6883d8da15da
Raphael Schwinger <raphaelschwinger@gmail.com>
2024-04-23 14:23:49 +0000

    fix merge

--- birdset/modules/metrics/multiclass.py
+++ birdset/modules/metrics/multiclass.py
@@ -31,7 +31,7 @@ class MulticlassMetricsConfig:
         self.val_metric_best: torchmetrics.Metric =torchmetrics.MaxMetric()
      
         self.add_metrics: torchmetrics.MetricCollection = torchmetrics.MetricCollection({
-            'F1': torchmetrics.F1Score(
+            'F2': torchmetrics.F1Score(
                 task="multiclass",
                 num_classes=num_labels,
             ),
@@ -50,46 +50,6 @@ class MulticlassMetricsConfig:
                 average='macro',
                 thresholds=None
             )
-        })
-        self.eval_complete: torchmetrics.MetricCollection = torchmetrics.MetricCollection({
-            'acc': torchmetrics.Accuracy(
-            task="multiclass",
-            num_classes=num_labels,
-        )
-        })
-
-class MulticlassMetricsConfig:
-    """
-    A class for configuring multiclass metrics used during model training and evaluation.
-
-    Attributes:
-        main_metric (Metric): The main metric used for model training.
-        val_metric_best (Metric): The metric used for model validation.
-        add_metrics (MetricCollection): A collection of additional metrics used during model training.
-        eval_complete (MetricCollection): A collection of metrics used during model evaluation.
-    """
-
-    def __init__(
-        self,
-        num_labels: int = 21,
-    ):
-        """
-        Initializes the MetricsConfig class.
-
-        Args:
-            num_labels (int): The number of labels in the dataset. Defaults to 21 as in the HSN dataset.
-        """
-        self.main_metric: torchmetrics.Metric = torchmetrics.Accuracy(
-            task="multiclass",
-            num_classes=num_labels,
-        )
-        self.val_metric_best: torchmetrics.Metric =torchmetrics.MaxMetric()
-     
-        self.add_metrics: torchmetrics.MetricCollection = torchmetrics.MetricCollection({
-            'F1': torchmetrics.F1Score(
-                task="multiclass",
-                num_classes=num_labels,
-            ),
         })
         self.eval_complete: torchmetrics.MetricCollection = torchmetrics.MetricCollection({
             'acc': torchmetrics.Accuracy(
