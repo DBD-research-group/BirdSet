65500592998e8f014c5193b42be45e58dd0caca9
XgamerTV <2015xgamer@gmail.com>
2024-05-10 15:47:38 +0000

    Readd label_counts

--- birdset/train.py
+++ birdset/train.py
@@ -74,6 +74,7 @@ def train(cfg):
         num_epochs=cfg.trainer.max_epochs, #?
         len_trainset=datamodule.len_trainset,
         batch_size=datamodule.loaders_config.train.batch_size,
+        label_counts=datamodule.num_train_labels,
         pretrain_info=pretrain_info
     )
 
