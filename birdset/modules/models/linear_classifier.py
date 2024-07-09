fb8d34645f8449f410e6a02b2ce74900ecea0062
Raphael Schwinger <raphaelschwinger@gmail.com>
2024-07-09 10:00:26 +0200

    Update birdset/modules/models/linear_classifier.py

--- birdset/modules/models/linear_classifier.py
+++ birdset/modules/models/linear_classifier.py
@@ -18,14 +18,6 @@ class LinearClassifier(nn.Module):
         """
         super().__init__()
         self.classifier = nn.Linear(in_features, num_classes)
-        '''self.classifier = nn.Sequential(
-            nn.Linear(in_features, 128),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, num_classes),
-        )'''
         
     
     def forward(
