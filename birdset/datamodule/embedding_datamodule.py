Padding fix, softmax fix

birdset/datamodule/embedding_datamodule.py
@@ -263,10 +263,7 @@ class EmbeddingDataModule(BaseDataModuleHF):
         current_num_samples = audio.shape[0]
         padding = desired_num_samples - current_num_samples
         if padding > 0:
-            #print('padding')
-            pad_left = padding // 2
-            pad_right = padding - pad_left
-            audio = torch.nn.functional.pad(audio, (pad_left, pad_right))
+            audio = torch.nn.functional.pad(audio, (0, padding))
         return audio
 
     # Average multiple embeddings function

birdset/modules/finetune_module.py
@@ -16,7 +16,10 @@ class FinetuneModule(BaseModule):
     def __init__(
             self,
             network: NetworkConfig = NetworkConfig(),
-            output_activation: Callable[[torch.Tensor], torch.Tensor] = torch.sigmoid,
+            output_activation: Callable[[torch.Tensor], torch.Tensor] = partial(
+                torch.softmax,
+                dim=1
+            ),
             loss: _Loss = CrossEntropyLoss(),
             optimizer: partial[Type[Optimizer]] = partial(
                 AdamW,
@@ -90,5 +93,6 @@ class FinetuneModule(BaseModule):
         embeddings, _ = self.embedding_model.get_embeddings(input_values)
 
         # Pass embeddings through the classifier to get the final output
-        embeddings = embeddings.view(embeddings.size(0), -1) # Transform for the classifier
-        return self.model.forward(embeddings)    
+        embeddings = embeddings.unsqueeze(1) # Transform for the classifier
+        logits = self.model.forward(embeddings)    
+        return logits