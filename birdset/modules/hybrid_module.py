6bc6a174ff296bd01c798d1bcc771f96356e3d35
XgamerTV <2015xgamer@gmail.com>
2024-09-17 23:43:45 +0000

    Current hybrid module state (WIP)

--- birdset/modules/hybrid_module.py
+++ birdset/modules/hybrid_module.py
@@ -17,7 +17,10 @@ class HybridModule(FinetuneModule):
     def __init__(
             self,
             network: NetworkConfig = NetworkConfig(),
-            output_activation: Callable[[torch.Tensor], torch.Tensor] = torch.sigmoid,
+            output_activation: Callable[[torch.Tensor], torch.Tensor] = partial(
+                torch.softmax,
+                dim=1
+            ),
             loss: _Loss = CrossEntropyLoss(),
             optimizer: partial[Type[Optimizer]] = partial(
                 AdamW,
@@ -34,12 +37,8 @@ class HybridModule(FinetuneModule):
             num_gpus: int = 1,
             pretrain_info = None,
             embedding_model: EmbeddingModuleConfig = EmbeddingModuleConfig(), # Model for extracting the embeddings
-            ft_optimizer: partial[Type[Optimizer]] = partial( # Optimizer to use for fine-tuning
-                AdamW,
-                lr=1e-4,
-                weight_decay=0.01,
-            ),
-            ft_max_epochs: int = 5,
+            #ft_lr: int = 5,
+            #ft_max_epochs: int = 5,
             ):
         super().__init__(
             network = network,
@@ -58,22 +57,17 @@ class HybridModule(FinetuneModule):
             embedding_model=embedding_model
         )
         self.linear_probing = True
-        self.ft_optimizer = ft_optimizer
-        self.ft_max_epochs = ft_max_epochs
+        #self.ft_lr = ft_lr
+        #self.ft_max_epochs = ft_max_epochs
         
-    def configure_optimizers(self):
-        if self.linear_probing: 
-            #print("PROBING")
-            self.embedding_model.eval()
-            for param in self.embedding_model.parameters(): # Just to be sure
-                param.requires_grad = False
-            self.optimizer = self.optimizer(list(self.model.parameters()))   
-        else:
-            #print("TUNING")
-            self.embedding_model.train()  
-            for param in self.embedding_model.parameters():
-                param.requires_grad = True 
-            self.optimizer = self.ft_optimizer(list(self.model.parameters())+list(self.embedding_model.parameters()))
+    """def configure_optimizers(self):
+        if not self.linear_probing:
+            return self.optimizer
+        
+        #self.embedding_model.eval()
+        for param in self.embedding_model.parameters(): # Just to be sure
+            param.requires_grad = False  
+        self.optimizer = self.optimizer(list(self.model.parameters())+list(self.embedding_model.parameters()))
         
         #! REMOVE OR CHECK THIS SCHEDULER THING
         '''    if self.lr_scheduler is not None:
@@ -98,25 +92,67 @@ class HybridModule(FinetuneModule):
 
                 return {"optimizer": self.optimizer, "lr_scheduler": scheduler_dict}'''
 
+        return {"optimizer": self.optimizer}"""
+
+    def configure_optimizers(self):
+        if not self.linear_probing:
+            self.embedding_model.train()  
+            #self.optimizer.add_param_group({'params': self.embedding_model.parameters()})
+
+            for param in self.embedding_model.parameters():
+                param.requires_grad = True 
+                
+            for param_group in self.trainer.optimizers[0].param_groups:
+                param_group['lr'] = 1e-4
+            
+            return self.optimizer
+        
+        self.optimizer = self.optimizer(list(self.model.parameters())+list(self.embedding_model.parameters())) #! Changed this
+        for param in self.embedding_model.parameters():
+                param.requires_grad = False
+        if self.lr_scheduler is not None:
+            # TODO: Handle the case when we do not want warmup
+            num_training_steps = math.ceil((self.num_epochs * self.len_trainset) / self.batch_size * self.num_gpus)
+            num_warmup_steps = math.ceil(
+                    num_training_steps * self.lr_scheduler.warmup_ratio
+                )
+            # TODO: Handle the case when drop_last=True more explicitly   
+
+            self.scheduler = self.lr_scheduler.scheduler(
+                optimizer=self.optimizer,
+                num_training_steps=num_training_steps,
+                num_warmup_steps=num_warmup_steps,
+            )
+
+            scheduler_dict = {
+                "scheduler": self.scheduler,
+                "interval": self.lr_scheduler.interval,
+                "warmup_ratio":self.lr_scheduler.warmup_ratio}                      
+
+            return {"optimizer": self.optimizer, "lr_scheduler": scheduler_dict}
+
         return {"optimizer": self.optimizer}
 
 
     def on_train_end(self):
         if self.linear_probing:
-            #print("STARTING fine-tuning...")
+            print("STARTING fine-tuning...")
             self.linear_probing = False
-            self.trainer.fit_loop.max_epochs += self.ft_max_epochs
-            for param_group in self.optimizer.param_groups:
-                print(f"Learning Rate: {param_group['lr']}")
             #print(self.trainer.checkpoint_callback.best_model_path)
-            self.trainer.fit(self,  datamodule=self.trainer.datamodule)
+            #self.trainer.fit(self,  datamodule=self.trainer.datamodule)
+            
+            #self.embedding_model.train()  
+            #for param in self.embedding_model.parameters():
+                #param.requires_grad = True 
                 
-    def model_step(self, batch, batch_idx):
-        logits = self.forward(**batch)
-        if self.class_mask and (not self.pretrain_info.valid_test_only or not self.trainer.training):
-            if batch["labels"].shape == logits.shape:
-                batch["labels"] = batch["labels"][:, self.class_mask]
-            logits = logits[:, self.class_mask]
-        loss = self.loss(logits, batch["labels"])
-        preds = self.output_activation(logits)
-        return loss, preds, batch["labels"]            
+            #for param_group in self.trainer.optimizers[0].param_groups:
+                #param_group['lr'] = 1e-4 # Set your new learning rate here 
+            #print("Erhöht")
+            
+            self.trainer.fit_loop.max_epochs += 5
+            
+            
+            self.trainer.fit(self,  datamodule=self.trainer.datamodule, ckpt_path=self.trainer.checkpoint_callback.best_model_path)         
+            #self.trainer.fit_loop.min_epochs = self.trainer.current_epoch + 5  
+            #self.trainer.fit(self,  datamodule=self.trainer.datamodule, ckpt_path=self.trainer.checkpoint_callback.best_model_path)         
+                        
