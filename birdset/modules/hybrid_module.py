5bc85feeb4dcf2ecd3565d8ab4d2422b727124e0
XgamerTV <2015xgamer@gmail.com>
2024-09-23 11:10:31 +0000

    Current hybrid experiments

--- birdset/modules/hybrid_module.py
+++ birdset/modules/hybrid_module.py
@@ -3,11 +3,12 @@ from functools import partial
 from typing import Callable, Literal, Type, Optional, Union
 from torch.nn import CrossEntropyLoss
 from torch.nn.modules.loss import _Loss
-from torch.optim import AdamW, Optimizer
+from torch.optim import AdamW, Optimizer, Adam
 from birdset.configs import NetworkConfig, LoggingParamsConfig, LRSchedulerConfig, MulticlassMetricsConfig, MultilabelMetricsConfig, MultilabelMetricsConfig as MetricsConfig
 from birdset.datamodule.embedding_datamodule import EmbeddingModuleConfig
 from birdset.utils import pylogger
 from birdset.modules.finetune_module import FinetuneModule
+from torch.optim.lr_scheduler import LambdaLR
 
 import torch
 import math
@@ -56,89 +57,66 @@ class HybridModule(FinetuneModule):
             pretrain_info = pretrain_info,
             embedding_model=embedding_model
         )
-        self.linear_probing = True
+        self.embedding_model.train()
+        #self.linear_probing = True
         self.ft_lr = ft_lr
         self.ft_max_epochs = ft_max_epochs
+        # Freeze the embedding model at the start
+        self.freeze_embedding_model()
         
-    """def configure_optimizers(self):
-        if not self.linear_probing:
-            return self.optimizer
-        
-        #self.embedding_model.eval()
-        for param in self.embedding_model.parameters(): # Just to be sure
-            param.requires_grad = False  
-        self.optimizer = self.optimizer(list(self.model.parameters())+list(self.embedding_model.parameters()))
-        
-        #! REMOVE OR CHECK THIS SCHEDULER THING
-        '''    if self.lr_scheduler is not None:
-                print("HALLO")
-                # TODO: Handle the case when we do not want warmup
-                num_training_steps = math.ceil((self.num_epochs * self.len_trainset) / self.batch_size * self.num_gpus)
-                num_warmup_steps = math.ceil(
-                        num_training_steps * self.lr_scheduler.warmup_ratio
-                    )
-                # TODO: Handle the case when drop_last=True more explicitly   
-
-                self.scheduler = self.lr_scheduler.scheduler(
-                    optimizer=self.optimizer,
-                    num_training_steps=num_training_steps,
-                    num_warmup_steps=num_warmup_steps,
-                )
-
-                scheduler_dict = {
-                    "scheduler": self.scheduler,
-                    "interval": self.lr_scheduler.interval,
-                    "warmup_ratio":self.lr_scheduler.warmup_ratio}                      
-
-                return {"optimizer": self.optimizer, "lr_scheduler": scheduler_dict}'''
-
-        return {"optimizer": self.optimizer}"""
-
     def configure_optimizers(self):
-        if not self.linear_probing:
-            self.embedding_model.train()  
-            self.optimizer.add_param_group({'params': self.embedding_model.parameters()})
-
-            for param in self.embedding_model.parameters():
-                param.requires_grad = True 
-                
-            for param_group in self.trainer.optimizers[0].param_groups:
-                param_group['lr'] = self.ft_lr
-            
-            return self.optimizer
-        
-        self.optimizer = self.optimizer(list(self.model.parameters())) #! Changed this
-        if self.lr_scheduler is not None:
-            # TODO: Handle the case when we do not want warmup
-            num_training_steps = math.ceil((self.num_epochs * self.len_trainset) / self.batch_size * self.num_gpus)
-            num_warmup_steps = math.ceil(
-                    num_training_steps * self.lr_scheduler.warmup_ratio
-                )
-            # TODO: Handle the case when drop_last=True more explicitly   
-
-            self.scheduler = self.lr_scheduler.scheduler(
-                optimizer=self.optimizer,
-                num_training_steps=num_training_steps,
-                num_warmup_steps=num_warmup_steps,
-            )
-
-            scheduler_dict = {
-                "scheduler": self.scheduler,
-                "interval": self.lr_scheduler.interval,
-                "warmup_ratio":self.lr_scheduler.warmup_ratio}                      
-
-            return {"optimizer": self.optimizer, "lr_scheduler": scheduler_dict}
-
+        # Set the AdamW optimizer with the initial learning rate
+        self.optimizer = self.optimizer(self.model.parameters())
+        #self.optimizer = Adam(self.model.parameters(), lr=1e-2) 
+        # Attach the learning rate scheduler
         return {"optimizer": self.optimizer}
+        #optimizer = self.optimizer([{'name':'classifier','params': list(self.model.parameters())+list(self.embedding_model.parameters()), 'lr': 1e-2}])#, {'name':'embedding','params': self.embedding_model.parameters(), 'lr': 0.0}])
 
+        # Custom scheduler: starts with warmup, then reduces to 0, and gradually increases after unfreezing
+        '''def lr_lambda(epoch):
+            if epoch < self.ft_max_epochs:
+                # Linearly increase the learning rate during warmup
+                return 1.0
+            elif epoch == self.ft_max_epochs:
+                # Reduce LR to zero after warmup
+                return 0.0
+            else:
+                # Gradually increase LR from 0 to max_lr after unfreezing
+                total_epochs = self.trainer.max_epochs
+                progress = (epoch - self.ft_max_epochs+1) / (total_epochs - self.ft_max_epochs+1)
+                return progress * (self.ft_lr / 1e-2)
+        
+        scheduler = LambdaLR(optimizer, lr_lambda=lr_lambda)
+        return [optimizer], [scheduler]'''
 
-    def on_train_end(self):
-        if self.linear_probing:
-            self.linear_probing = False
-            self.trainer.fit_loop.max_epochs += self.ft_max_epochs
-            # Load best model
-            best_model_path = self.trainer.checkpoint_callback.best_model_path
-            state_dict = torch.load(best_model_path)["state_dict"]
-            self.load_state_dict(state_dict, strict=True)
-            log.info("Starting finetuning and loaded best model")
-            self.trainer.fit(self,  datamodule=self.trainer.datamodule)         
+    def on_train_epoch_start(self):
+        #for param_group in self.optimizer.param_groups:
+            #print(self.current_epoch,param_group['lr'])
+        # Change the learning rate after epoch 15
+        if self.current_epoch == self.ft_max_epochs:
+            print(f"Changing learning rate to {self.ft_lr}")
+            self.unfreeze_embedding_model()    
+            #optimizer = self.optimizers()
+            #optimizer.add_param_group({'name':'embedding','params': self.embedding_model.parameters(), 'lr': 0.0})
+            #for param_group in optimizer.param_groups:
+                #param_group['lr'] = self.ft_lr
+            
+            
+            
+            optimizer = AdamW([
+                {'params': self.model.parameters(), 'lr': self.ft_lr, 'weight_decay': 5e-4},  # Lower LR for feature extractor
+                {'params': self.embedding_model.parameters(), 'lr': self.ft_lr, 'weight_decay': 5e-4}    # Higher LR for classifier head
+            ])
+            self.trainer.optimizers = [optimizer]
+        #print(self.lr_schedulers())
+        print(self.optimizers())  
+
+    def freeze_embedding_model(self):
+        """Freeze the embedding model's parameters."""
+        for param in self.embedding_model.parameters():
+            param.requires_grad = False
+            
+    def unfreeze_embedding_model(self):
+        """Unfreeze the embedding model's parameters."""
+        for param in self.embedding_model.parameters():
+            param.requires_grad = True    
