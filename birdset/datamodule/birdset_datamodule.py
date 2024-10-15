1802ad76eeb30ab76dfed564d04eb8d5818f7eeb
Raphael Schwinger <raphaelschwinger@gmail.com>
2024-10-15 10:00:40 +0200

    reduce batch_size of one-hot encoding

--- birdset/datamodule/birdset_datamodule.py
+++ birdset/datamodule/birdset_datamodule.py
@@ -155,7 +155,7 @@ class BirdSetDataModule(BaseDataModuleHF):
                 dataset[split] = dataset[split].map(
                     self._classes_one_hot,
                     batched=True,
-                    batch_size=1500,
+                    batch_size=300,
                     load_from_cache_file=True,
                     num_proc=self.dataset_config.n_workers,
                     desc=f"One-hot-encoding {split} labels."
