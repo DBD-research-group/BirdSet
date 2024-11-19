77c082f16643322851f345d2b32d6570c548d389
PariaValizadeh <pariya.valizadeh@gmail.com>
2024-11-19 09:54:41 +0000

    fingerprint_for_CV_update

--- birdset/datamodule/esc50_datamodule.py
+++ birdset/datamodule/esc50_datamodule.py
@@ -7,7 +7,7 @@ from birdset.configs import DatasetConfig, LoadersConfig
 from datasets import load_dataset, load_from_disk, Audio, DatasetDict, Dataset, IterableDataset, IterableDatasetDict
 from birdset.utils import pylogger
 from datasets import Dataset, DatasetDict, concatenate_datasets
-
+import os
 log = pylogger.get_pylogger(__name__)
 
 class ESC50DataModule(BaseDataModuleHF):
@@ -30,6 +30,7 @@ class ESC50DataModule(BaseDataModuleHF):
         )
         self.cross_valid = cross_valid
         self.fold = fold
+        print("foooooooold", self.fold)
     
 
     def prepare_data(self):
@@ -132,6 +133,35 @@ class ESC50DataModule(BaseDataModuleHF):
         # Return the cross-validated dataset
         return cv_dataset
 
+    def _save_dataset_to_disk(self, dataset: Dataset | DatasetDict):
+        """
+        Saves the dataset to disk.
+
+        This method sets the format of the dataset to numpy, prepares the path where the dataset will be saved, and saves
+        the dataset to disk. If the dataset already exists on disk, it does not save the dataset again.
+
+        Args:
+            dataset (datasets.DatasetDict): The dataset to be saved. The dataset should be a Hugging Face `datasets.DatasetDict` object.
+
+        Returns:
+            None
+        """
+        dataset.set_format("np")
+        if self.cross_valid:
+            fingerprint = f"{dataset[next(iter(dataset))]._fingerprint}_{self.fold}" if isinstance(dataset, DatasetDict) else f"{dataset._fingerprint}_{self.fold}"  # changed to next_iter to be more robust
+        else:
+            fingerprint = dataset[next(iter(dataset))]._fingerprint if isinstance(dataset, DatasetDict) else dataset._fingerprint  # changed to next_iter to be more robust
+
+        self.disk_save_path = os.path.join(
+            self.dataset_config.data_dir,
+            f"{self.dataset_config.dataset_name}_processed_{self.dataset_config.seed}_{fingerprint}",
+        )
+
+        if os.path.exists(self.disk_save_path):
+            log.info(f"Train fingerprint found in {self.disk_save_path}, saving to disk is skipped")
+        else:
+            log.info(f"Saving to disk: {self.disk_save_path}")
+            dataset.save_to_disk(self.disk_save_path)
 
     def _create_splits(self, dataset: DatasetDict | Dataset) -> DatasetDict:
         """
