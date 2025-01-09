6c48e2108ebcf26d403f0e2434c75499883a1965
Tom Hauschild <stu216509@mail.uni-kiel.de>
2025-01-09 14:31:55 +0000

    correct formatting

--- tests/test_fast_dev_runs.py
+++ tests/test_fast_dev_runs.py
@@ -1,19 +1,25 @@
 import subprocess
 import pytest
 
+
 def test_train_script():
     """
     Test the training script with fast development run enabled.
-    This function runs the training script using a subprocess with the 
-    `trainer.fast_dev_run=True` argument to quickly check for any issues 
-    in the training pipeline. It asserts that the script completes 
+    This function runs the training script using a subprocess with the
+    `trainer.fast_dev_run=True` argument to quickly check for any issues
+    in the training pipeline. It asserts that the script completes
     successfully without errors.
     Raises:
-        pytest.fail: If the training script fails to run successfully, 
+        pytest.fail: If the training script fails to run successfully,
         this exception is raised with the error message from the subprocess.
     """
-    
-    command = ["python", "birdset/train.py", 'experiment="local/DT_example.yaml"', 'trainer.fast_dev_run=True']
+
+    command = [
+        "python",
+        "birdset/train.py",
+        'experiment="local/DT_example.yaml"',
+        "trainer.fast_dev_run=True",
+    ]
     try:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
         assert result.returncode == 0
