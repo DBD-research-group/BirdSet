e151580ee85e104b457f45c4ed87c4fe2cbbdbc4
Tom Hauschild <stu216509@mail.uni-kiel.de>
2025-02-03 17:33:05 +0000

    init integration model and dataset tests

---
+++ tests/test_datasets.py
@@ -0,0 +1,25 @@
+import pytest
+from tests.utils import generate_commands, run_experiment_test
+
+DATASET_EXPERIMENTS = [
+    "birdset_neurips24/HSN/DT/convnext.yaml",
+    "birdset_neurips24/NBP/DT/convnext.yaml",
+    "birdset_neurips24/NES/DT/convnext.yaml",
+    "birdset_neurips24/PER/DT/convnext.yaml",
+    "birdset_neurips24/POW/DT/convnext.yaml",
+    "birdset_neurips24/SNE/DT/convnext.yaml",
+    "birdset_neurips24/SSW/DT/convnext.yaml",
+    "birdset_neurips24/UHH/DT/convnext.yaml",
+    "birdset_neurips24/XCL/convnext.yaml",
+    "birdset_neurips24/XCM/convnext.yaml",
+]
+
+commands = generate_commands(DATASET_EXPERIMENTS)
+
+@pytest.mark.parametrize(
+    "experiment,command",
+    commands.items(),
+    ids=["/".join(exp.split("/")[-3:]) for exp in commands.keys()],
+)
+def test_dataset_experiment(experiment, command):
+    run_experiment_test(experiment, command)
---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+import pytest
+from tests.utils import generate_commands, run_experiment_test
+
+MODEL_EXPERIMENTS = [
+    "birdset_neurips24/HSN/DT/ast.yaml",
+    "birdset_neurips24/HSN/DT/convnext.yaml",
+    "birdset_neurips24/HSN/DT/eat.yaml",
+    "birdset_neurips24/HSN/DT/efficientnet.yaml",
+    "birdset_neurips24/HSN/DT/wav2vec2.yaml",
+]
+
+commands = generate_commands(MODEL_EXPERIMENTS)
+
+@pytest.mark.parametrize(
+    "experiment,command",
+    commands.items(),
+    ids=["/".join(exp.split("/")[-3:]) for exp in commands.keys()],
+)
+def test_model_experiment(experiment, command):
+    run_experiment_test(experiment, command)
