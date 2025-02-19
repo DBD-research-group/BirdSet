139f512cdf3159485aca41bf113dd7e43dd5453e
Raphael Schwinger <raphaelschwinger@gmail.com>
2025-02-19 09:27:02 +0000

    add flag to use test dataset in validation

--- birdset/datamodule/base_datamodule.py
+++ birdset/datamodule/base_datamodule.py
@@ -363,6 +363,8 @@ class BaseDataModuleHF(L.LightningDataModule):
                 log.info("fit")
                 self.val_dataset = self._get_dataset("valid")
                 self.train_dataset = self._get_dataset("train")
+            if self.dataset_config.use_test_as_valid:
+                self.test_dataset = self._get_dataset("test")
 
         if not self.test_dataset:
             if stage == "test":
@@ -497,7 +499,10 @@ class BaseDataModuleHF(L.LightningDataModule):
             return DataLoader(self.train_dataset, **asdict(self.loaders_config.train))  # type: ignore
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, **asdict(self.loaders_config.valid))  # type: ignore
+        if self.dataset_config.use_test_as_valid:
+            return DataLoader(self.test_dataset, **asdict(self.loaders_config.valid))
+        else:
+            return DataLoader(self.val_dataset, **asdict(self.loaders_config.valid))  # type: ignore
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, **asdict(self.loaders_config.test))  # type: ignore
